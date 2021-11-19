120a73a81a052f1becaea88c23fbbbd8063d9fa2
REDIZIT <38188793+REDIZIT@users.noreply.github.com>
2021-11-19 15:39:59 +0300

    Version text

---
+++ Assets/Scripts/InApp/VersionUI.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace InApp
+{
+    public class VersionUI : MonoBehaviour
+    {
+        [SerializeField] private Text versionText;
+
+        private const string REPO_URL = "https://github.com/REDIZIT/CIAN-Excel-Exporter/releases/tag/release";
+
+        private void Start()
+        {
+            versionText.text = Application.version;
+        }
+        public void OpenGithub()
+        {
+            Application.OpenURL(REPO_URL);
+        }
+    }
+}
